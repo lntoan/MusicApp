333a17c45182ab02ce9eefe196d56099f7f84142
lntoan <lntoan@gmail.com>
2017-08-07 13:40:40 +0700

    First comit

---
+++ src/actions.js
@@ -0,0 +1,25 @@
+import { search, streamUrl } from './soundcloudHelper';
+
+import { ReactNativeAudioStreaming, Player } from 'react-native-audio-streaming';
+
+export const playingGenre = (genre) => ({
+    type: 'PLAYING_GENRE',
+    genre
+});
+
+export const playGenre = (genre) => {
+    return function (dispatch) {
+        dispatch(playingGenre(genre));
+
+        search(genre.name)
+          .then(result => {
+              dispatch(foundSongs(result.collection, genre));
+          });
+    }
+};
+
+export const foundSongs = (songs, genre) => ({
+    type: 'FOUND_SONGS',
+    songs,
+    genre
+});
---
+++ src/reducer.js
@@ -0,0 +1,42 @@
+import { combineReducers } from 'redux';
+
+const initialState = {
+    genres: ["Alternative Rock", "Ambient", "Classical", "Country", "EDM",
+             "Dancehall", "Deep House", "Disco", "Drum & Bass", "Dubstep", "Electronic",
+             "Folk", "Singer-Songwriter", "Rap", "House", "Indie", "Jazz & Blues",
+             "Latin", "Metal", "Piano", "Pop", "R&B & Soul", "Reggae", "Reggaeton",
+             "Rock", "Soundtrack", "Techno", "Trance", "Trap", "Triphop"].map(
+                 (name, i) => ({
+                     id: i,
+                     name
+                 })
+             ),
+    currentlyPlaying: {
+        id: null,
+        name: null
+    },
+    songs: {}
+};
+
+const rootReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'PLAYING_GENRE':
+            return Object.assign({}, state,
+                                 {
+                                     currentlyPlaying: action.genre
+                                 });
+        case 'FOUND_SONGS':
+            const { songs, genre } = action;
+            return Object.assign({}, state,
+                                 {
+                                     songs: Object.assign({}, state.songs,
+                                                          {
+                                                              [genre.id]: songs
+                                                          })
+                                 })
+        default:
+            return state;
+    }
+};
+
+export default rootReducer;
